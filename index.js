4c1e2af139a9b0dcaa74c5a5d4875216b5731db9
Doriane <berdalado@gmail.com>
2021-03-22 08:31:59 +0100

    first node

---
+++ index.js
@@ -0,0 +1,7 @@
+const myInformation = require('./information.js');
+myInformation();
+
+const cowsay = require('cowsay');
+console.log(cowsay.say({
+  text: 'I am Doriane from Lyon'
+}))
---
+++ information.js
@@ -0,0 +1,8 @@
+let name = 'Dori';
+let campus = 'Lyon';
+
+const myInfos = () => {
+  console.log(`Hi my name is ${name}, I'm from  ${campus}`);
+};
+
+module.exports = myInfos;
